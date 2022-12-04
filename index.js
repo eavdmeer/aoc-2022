fd8bf756d95422a2d763c55ef1e028fd0320b272
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-04 14:30:24 +0100

    Show results sorted by day

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import day2 from './day2/index.js';
 import day3 from './day3/index.js';
 import day4 from './day4/index.js';
 
-day1('day1/data.txt').then(console.log);
-day2('day2/data.txt').then(console.log);
-day3('day3/data.txt').then(console.log);
-day4('day4/data.txt').then(console.log);
+async function getResults()
+{
+  const result = [
+    await day1('day1/data.txt'),
+    await day2('day2/data.txt'),
+    await day3('day3/data.txt'),
+    await day4('day4/data.txt')
+  ];
+  return result.sort((a, b) => a.day - b.day);
+}
+
+getResults().then(console.log);
