88f97b5e9d64b0c3995980c679b9e88529a6c6c1
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-04 09:36:18 +0100

    Use promise-based code

--- day1/index.js
+++ day1/index.js
@@ -1,27 +1,31 @@
-const fs = require('fs');
+import * as fs from 'node:fs/promises';
 
 const input = process.argv[2] || 'data.txt';
 
-fs.readFile(input, (err, content) =>
+export default function day1()
 {
-  if (err)
-  {
-    console.log(err.message);
-    return;
-  }
-  const totals = content
-    .toString()
-    .split('\n\n')
-    .map(v => v
-      .split(/\s*\n\s*/)
-      .filter(w => w !== '')
-      .reduce((a, w) => a + parseInt(w, 10), 0)
-    );
+  fs.readFile(input)
+    .then(content =>
+    {
+      const totals = content
+        .toString()
+        .split('\n\n')
+        .map(v => v
+          .split(/\s*\n\s*/)
+          .filter(w => w !== '')
+          .reduce((a, w) => a + parseInt(w, 10), 0)
+        );
 
-  const max = Math.max(...totals);
-  console.log(totals.indexOf(max) + 1, max);
+      const max = Math.max(...totals);
+      console.log(totals.indexOf(max) + 1, max);
 
-  const top3 = totals.sort().reverse().slice(0, 3).reduce((a, v) => a + v, 0);
-  console.log(top3);
+      const top3 = totals
+        .sort()
+        .reverse()
+        .slice(0, 3)
+        .reduce((a, v) => a + v, 0);
+      console.log(top3);
 
-});
+    })
+    .catch(err => console.log(err));
+}
