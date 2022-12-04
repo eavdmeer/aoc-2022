09fef63abf302219d2838788125075beec7b0e54
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-04 09:59:42 +0100

    Convert day3 and day4 to promise and modules

--- day3/index.js
+++ day3/index.js
@@ -1,46 +1,47 @@
-const fs = require('fs');
+import * as fs from 'node:fs/promises';
 
-const input = process.argv[2] || 'data.txt';
-
-fs.readFile(input, (err, content) =>
+if (process.argv[2])
 {
-  if (err)
-  {
-    console.log(err.message);
-    return;
-  }
+  day3(process.argv[2] || 'data.txt');
+}
 
-  console.log('part1:',
-    content
-      .toString()
-      .split(/\s*\n\s*/)
-      .filter(v => v)
-      .map(v => [ v.substring(0, v.length / 2), v.substring(v.length / 2) ])
-      .map(([ l, r ]) => l.split('').filter(v => r.includes(v)).pop())
-      .map(v => v.charCodeAt(0) - 96)
-      .map(v => v < 0 ? v + 58 : v)
-      .reduce((a, v) => a + v, 0)
-  );
+export default function day3(target)
+{
+  fs.readFile(target)
+    .then(content =>
+    {
+      const part1 = content
+        .toString()
+        .split(/\s*\n\s*/)
+        .filter(v => v)
+        .map(v => [ v.substring(0, v.length / 2), v.substring(v.length / 2) ])
+        .map(([ l, r ]) => l.split('').filter(v => r.includes(v)).pop())
+        .map(v => v.charCodeAt(0) - 96)
+        .map(v => v < 0 ? v + 58 : v)
+        .reduce((a, v) => a + v, 0);
 
-  const elves = content
-    .toString()
-    .split(/\s*\n\s*/)
-    .filter(v => v);
+      const elves = content
+        .toString()
+        .split(/\s*\n\s*/)
+        .filter(v => v);
 
-  const groupSize = 3;
-  const groups = [];
-  for (let i = 0; i < elves.length; i += groupSize)
-  {
-    groups.push(elves.slice(i, i + groupSize));
-  }
+      const groupSize = 3;
+      const groups = [];
+      for (let i = 0; i < elves.length; i += groupSize)
+      {
+        groups.push(elves.slice(i, i + groupSize));
+      }
 
-  console.log('part2',
-    groups
-      .map(([ x, y, z ]) => x.split('')
-        .filter(v => y.includes(v) && z.includes(v))
-        .pop())
-      .map(v => v.charCodeAt(0) - 96)
-      .map(v => v < 0 ? v + 58 : v)
-      .reduce((a, v) => a + v, 0)
-  );
-});
+      const part2 = groups
+        .map(([ x, y, z ]) => x.split('')
+          .filter(v => y.includes(v) && z.includes(v))
+          .pop())
+        .map(v => v.charCodeAt(0) - 96)
+        .map(v => v < 0 ? v + 58 : v)
+        .reduce((a, v) => a + v, 0);
+
+      console.log('day3:', { part1, part2 });
+    })
+    .catch(err => console.log(err.message));
+}
+
--- day4/index.js
+++ day4/index.js
@@ -1,50 +1,50 @@
-const fs = require('fs');
+import * as fs from 'node:fs/promises';
 
-const input = process.argv[2] || 'data.txt';
-
-fs.readFile(input, (err, content) =>
+if (process.argv[2])
 {
-  if (err)
-  {
-    console.log(err.message);
-    return;
-  }
+  day4(process.argv[2] || 'data.txt');
+}
 
-  console.log('part1:',
-    content
-      .toString()
-      .split(/\s*\n\s*/)
-      .filter(v => v)
-      .map(v => v
-        .split(',')
-        .map(w => w
-          .split('-')
-          .map(x => parseInt(x, 10))
+export default function day4(target)
+{
+  fs.readFile(target)
+    .then(content =>
+    {
+      const part1 = content
+        .toString()
+        .split(/\s*\n\s*/)
+        .filter(v => v)
+        .map(v => v
+          .split(',')
+          .map(w => w
+            .split('-')
+            .map(x => parseInt(x, 10))
+          )
         )
-      )
-      .filter(([ l, r ]) =>
-        l[0] >= r[0] && l[1] <= r[1] ||
-        r[0] >= l[0] && r[1] <= l[1]
-      )
-      .length
-  );
+        .filter(([ l, r ]) =>
+          l[0] >= r[0] && l[1] <= r[1] ||
+            r[0] >= l[0] && r[1] <= l[1]
+        )
+        .length;
 
-  console.log('part2:',
-    content
-      .toString()
-      .split(/\s*\n\s*/)
-      .filter(v => v)
-      .map(v => v
-        .split(',')
-        .map(w => w
-          .split('-')
-          .map(x => parseInt(x, 10))
-        )
-      )
-      .filter(([ l, r ]) =>
-        l[0] >= r[0] && l[0] <= r[1] ||
-        r[0] >= l[0] && r[0] <= l[1]
-      )
-      .length
-  );
-});
+      const part2 =
+        content
+          .toString()
+          .split(/\s*\n\s*/)
+          .filter(v => v)
+          .map(v => v
+            .split(',')
+            .map(w => w
+              .split('-')
+              .map(x => parseInt(x, 10))
+            )
+          )
+          .filter(([ l, r ]) =>
+            l[0] >= r[0] && l[0] <= r[1] ||
+            r[0] >= l[0] && r[0] <= l[1]
+          )
+          .length;
+      console.log('day4:', { part1, part2 });
+    })
+    .catch(err => console.log(err.message));
+}
