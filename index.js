dabe211e877afa96b21fe648d5f24162cc88deba
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-07 20:58:43 +0100

    Add day6 to the main script

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import day3 from './day3/index.js';
 import day4 from './day4/index.js';
 import day5 from './day5/index.js';
 import day6 from './day6/index.js';
+import day7 from './day7/index.js';
 
 async function getResults()
 {
@@ -13,7 +14,8 @@ async function getResults()
     await day3('day3/data.txt'),
     await day4('day4/data.txt'),
     await day5('day5/data.txt'),
-    await day6('day6/data.txt')
+    await day6('day6/data.txt'),
+    await day7('day7/data.txt')
   ];
   return result.sort((a, b) => a.day - b.day);
 }
