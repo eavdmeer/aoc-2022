f86368cae8dc3289ed369d37a33270d1085f83ce
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-14 13:57:01 +0100

    Run day14

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import day10 from './day10/index.js';
 import day11 from './day11/index.js';
 import day12 from './day12/index.js';
 import day13 from './day13/index.js';
+import day14 from './day14/index.js';
 
 async function getResults()
 {
@@ -27,7 +28,8 @@ async function getResults()
     await day10('day10/data.txt'),
     await day11('day11/data.txt'),
     await day12('day12/data.txt'),
-    await day13('day13/data.txt')
+    await day13('day13/data.txt'),
+    await day14('day14/data.txt')
   ];
   return result.sort((a, b) => a.day - b.day);
 }
