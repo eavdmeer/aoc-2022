2924120e60d05f13c7ff4574b02a8e6f5e133fd9
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-19 12:32:13 +0100

    Move Graph class to lib

--- day16/index.js
+++ day16/index.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs/promises';
-import Graph from './graph.js';
+import Graph from '../lib/graph.js';
 
 let doDebug = false;
 if (process.argv[2])
@@ -16,11 +16,19 @@ function debug(...args)
 
 function pop(heap)
 {
-  // pop the item on the heap with the highest volume and return it
-  const max = Math.max(...heap.map(v => v.volume));
-  return heap.splice(heap.findIndex(v => v.volume === max), 1).pop();
+  console.log(heap.map(v => v.volume + (30 - v.time) * v.valve.flow));
+
+  // pop the item on the heap with the highest potential volume of
+  // remaining time * flow rate and return it
+  const max = Math.max(...heap.map(v =>
+    v.volume + (30 - v.time) * v.valve.flow));
+  debug('max heap value is', max);
+  return heap.splice(heap
+    .findIndex(v => v.volume + (30 - v.time) * v.valve.flow === max), 1)
+    .pop();
 }
 
+/*
 function maxflow(nodes, node, opened, remaining)
 {
   debug('check node', node.id, 'remaining', remaining, 'opened', opened);
@@ -49,58 +57,72 @@ function maxflow(nodes, node, opened, remaining)
   debug('returning', best);
   return best;
 }
+*/
 
 function solve(valves, start)
 {
   const graph = new Graph(v => v.id);
-  const getValve = id => valves.find(v => v.id === id);
+  const getValve = id => withFlow.find(v => v.id === id);
 
   // Insert all nodes
   graph.insert(...valves);
 
   // Insert all edges
-  valves.forEach(v => v.to.forEach(d => graph.connect(v.id, d, v.flow)));
+  valves.forEach(v => v.to.forEach(d => graph.connect(v.id, d)));
+
+  // Get all valves with non-zero flow
+  const withFlow = valves
+    .filter(v => v.id === 'AA' || v.flow > 0)
+    .map(v => ({ id: v.id, flow: v.flow }));
+  debug('valves with flow', withFlow);
+
+  // Add distances using the graph
+  withFlow.forEach(f1 =>
+  {
+    f1.dist = withFlow
+      .filter(v => v !== f1)
+      .map(f2 => [ f2.id, graph.findPath(f1.id, f2.id).cost ])
+      .reduce((a, [ k, v ]) => { a[k] = v; return a; }, {});
+  });
+  debug('valves with distances', withFlow);
 
   const heap = [];
-  const visited = {};
-  const opened = {};
-  heap.push({ ...getValve(start) });
+  const visited = new Set();
 
-  let t = 1;
-  while (heap.length > 0 && t <= 30)
+  // push start onto the heap
+  heap.push({ volume: 0, time: 0, valve: getValve(start) });
+
+  while (heap.length > 0)
   {
-    debug('heap', heap
-      .map(v => `${v.id}: ${v.id in opened}, ${v.volume}`)
-      .join('\n'));
-    const valve = pop(heap);
-    debug('try valve', valve.id, 'volume', valve.volume);
-    if (valve.id in visited)
+    debug('heap', heap);
+    const { volume, time, valve } = pop(heap);
+    debug('t =', time, ': try valve', valve.id, 'flow', valve.flow,
+      'volume', volume);
+    if (visited.has(valve.id))
     {
       debug('  already visited');
       continue;
     }
-    visited[valve.id] = true;
-    if (t === 30)
+    visited.add(valve.id);
+    if (visited.size === withFlow.length)
+    {
+      debug('  visited and opened all valves');
+      return volume;
+    }
+    if (time === 30)
     {
       debug('out of time');
       return valve.volume;
     }
-    debug('add valves', valve.to);
-    valve.to.forEach(v =>
-    {
-      const n = { ...getValve(v) };
-      n.volume = valve.volume;
-      heap.push(n);
-    });
-    if (! (valve.id in opened) && valve.flow !== 0)
-    {
-      // Only open valves that will yield anything
-      debug('open valve', valve.id, 'flow', valve.flow);
-      opened[valve.id] = t;
-      valve.volume += valve.flow * (30 - 1);
-      heap.push(valve);
-    }
-    t++;
+    debug('add valves', valve.dist);
+    debug('opening valve, flow', valve.flow, 'for', 30 - time, 'minutes for a total volume of', valve.flow * (30 - time));
+
+    Object.entries(valve.dist).forEach(([ id, dist ]) =>
+      heap.push({
+        volume: volume + valve.flow * (30 - time),
+        time: time + 1 + dist,
+        valve: getValve(id)
+      }));
   }
   debug('out of search options');
 
@@ -121,16 +143,16 @@ export default async function day16(target)
     .map(v => ({
       id: v[1],
       flow: parseInt(v[2], 10),
-      to: v[3].split(/\s*,\s*/),
-      volume: 0
+      to: v[3].split(/\s*,\s*/)
     }));
 
-  debug(valves);
+  debug('all valves', valves);
 
   debug(solve(valves, 'AA'));
 
-  const part1 = maxflow(valves, valves.find(v => v.id === 'AA'), {}, 30);
+  // const part1 = maxflow(valves, valves.find(v => v.id === 'AA'), {}, 30);
 
+  const part1 = 'todo';
   const part2 = 'todo';
 
   return { day: 16, part1, part2, duration: Date.now() - start };
