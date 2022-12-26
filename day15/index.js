1cd9f350efff45b59cadc7850445bfbd80ab55cf
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-26 07:29:15 +0100

    Clean up existing solution to prepare for part 2

--- day15/index.js
+++ day15/index.js
@@ -1,52 +1,15 @@
 import * as fs from 'node:fs/promises';
+import makeDebug from 'debug';
 
-let doDebug = false;
+const debug = makeDebug('day15');
+
 if (process.argv[2])
 {
-  doDebug = process.argv[2].includes('example');
   day15(process.argv[2]).then(console.log);
 }
 
-function debug(...args)
+function solve1(sensors, yval)
 {
-  if (! doDebug) { return; }
-  console.log(...args);
-}
-
-export default async function day15(target)
-{
-  const start = Date.now();
-
-  const content = await fs.readFile(target);
-
-  const re = /^.* at x=(-?\d+), y=(-?\d+): .* at x=(-?\d+), y=(-?\d+)$/;
-
-  const manhatten = (x1, y1, x2, y2) => Math.abs(x2 - x1) + Math.abs(y2 - y1);
-
-  const sensors = content
-    .toString()
-    .split(/\s*\n\s*/)
-    .filter(v => v)
-    .map(v => v.match(re))
-    .map((v, i) => ({
-      id: i,
-      x: parseInt(v[1], 10),
-      y: parseInt(v[2], 10),
-      nearest: {
-        x: parseInt(v[3], 10),
-        y: parseInt(v[4], 10)
-      }
-    }))
-    .map(v =>
-    {
-      v.radius = manhatten(v.x, v.y, v.nearest.x, v.nearest.y);
-      return v;
-    });
-
-  debug(sensors);
-
-  const yval = target.includes('example') ? 10 : 2000000;
-
   // x-interval in range for a sensor for a certain y value
   const range = (sensor, y) =>
   {
@@ -81,10 +44,13 @@ export default async function day15(target)
   reduced.push([ min, max ]);
   debug('reduced:', reduced);
 
-  const part1 = reduced
+  return reduced
     .map(v => v[1] - v[0])
     .reduce((a, v) => a + v, 0);
+}
 
+function solve2(sensors)
+{
   /*
    * line equations of the bounding lines for each sensor
    *
@@ -96,8 +62,6 @@ export default async function day15(target)
    *  y = x + sensor.y - sensor.x + sensor.radius
    *  y = x + sensor.y - sensor.x - sensor.radius
    */
-  const part2 = 'todo';
-
   const down = [
     ...sensors.map(s => s.y + s.x + s.radius),
     ...sensors.map(s => s.y + s.x - s.radius) ];
@@ -107,6 +71,53 @@ export default async function day15(target)
     ...sensors.map(s => s.y - s.x + s.radius),
     ...sensors.map(s => s.y - s.x - s.radius) ];
   debug(up);
+  return 'todo';
+}
+
+export default async function day15(target)
+{
+  const start = Date.now();
+
+  const content = await fs.readFile(target);
+
+  const re = /^.* at x=(-?\d+), y=(-?\d+): .* at x=(-?\d+), y=(-?\d+)$/;
+  const manhatten = (x1, y1, x2, y2) => Math.abs(x2 - x1) + Math.abs(y2 - y1);
+
+  const sensors = content
+    .toString()
+    .split(/\s*\n\s*/)
+    .filter(v => v)
+    .map(v => v.match(re))
+    .map((v, i) => ({
+      id: i,
+      x: parseInt(v[1], 10),
+      y: parseInt(v[2], 10),
+      bx: parseInt(v[3], 10),
+      by: parseInt(v[4], 10)
+    }))
+    .map(v =>
+    {
+      debug(v.x, v.y, v.bx, v.by);
+      v.radius = manhatten(v.x, v.y, v.bx, v.by);
+      return v;
+    });
+
+  debug(sensors);
+
+  const yval = target.includes('example') ? 10 : 2000000;
+
+  const part1 = solve1(sensors, yval);
+  if (target.includes('example') && part1 !== 26)
+  {
+    throw new Error(`Invalid part 1 solution: ${part1}. Expecting; 26`);
+  }
+
+  const part2 = solve2(sensors);
+  if (target.includes('example') && part2 !== 'todo')
+  {
+    throw new Error(`Invalid part 2 solution: ${part2}. Expecting; 'todo'`);
+  }
+
 
   return { day: 15, part1, part2, duration: Date.now() - start };
 }
